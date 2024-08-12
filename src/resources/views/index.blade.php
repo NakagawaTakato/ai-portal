59be230dcc2fa9143b678cf7e87085486b0415a7
NakagawaTakato <ntakato6231@gmail.com>
2024-08-12 14:31:21 +0900

    2

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="container-group">
+        <a href="/shop_all" class="container-group-text_one">iMPRESS Inc</a>
+        <a href="/register" class="container-group-text_two">ホーム</a>
+        <a href="/login" class="container-group-text_three">会社情報</a>
+        <a href="/login" class="container-group-text_four">お問い合わせ</a>
+    </div>
+</div>
+@endsection
