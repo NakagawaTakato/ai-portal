cf7e1b663ba100cbbe6bfa6d8e902aae1d3df72c
NakagawaTakato <ntakato6231@gmail.com>
2024-09-16 14:57:17 +0900

    14

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
   <div class="login-form__heading">
     <h2>ログイン</h2>
   </div>
-  <form class="form" action="/" method="post">
+  <form class="form" action="/login" method="post">
     <div class="form__group">
       <div class="form__group-title">
         <span class="form__label--item">メールアドレス</span>
